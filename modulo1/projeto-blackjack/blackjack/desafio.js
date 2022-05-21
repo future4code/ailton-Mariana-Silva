d22a8ac42ad33241517f8157a3706e271045e16a
Mariana Andrade <104591781+Mariandr4de@users.noreply.github.com>
2022-05-20 21:32:19 -0300

    projeto blackjack

---
+++ modulo1/projeto-blackjack/blackjack/desafio.js
@@ -0,0 +1,13 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
