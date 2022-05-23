16df0914a29b9f8e92c4676c5bea00e409fda9a5
Mariana Andrade <104591781+Mariandr4de@users.noreply.github.com>
2022-05-23 15:36:07 -0300

    Exercicio 1

---
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -0,0 +1,89 @@
+// ATENÇÃO!!!
+//    -> NÃO COMENTE NENHUMA DAS FUNÇÕES DECLARADAS!!! 
+//    -> NÃO MODIFIQUE OS PARÂMETROS DAS FUNÇÕES!!! ()
+
+
+// EXERCÍCIO 01
+function retornaTamanhoArray(array) {
+    return array.length
+}
+
+// EXERCÍCIO 02
+function retornaArrayInvertido(array) {
+  
+}
+
+// EXERCÍCIO 03
+function retornaArrayOrdenado(array) {
+  
+}
+
+// EXERCÍCIO 04
+function retornaNumerosPares(array) {
+  
+}
+
+// EXERCÍCIO 05
+function retornaNumerosParesElevadosADois(array) {
+ 
+}
+
+// EXERCÍCIO 06
+function retornaMaiorNumero(array) {
+  
+}
+
+// EXERCÍCIO 07
+function retornaObjetoEntreDoisNumeros(num1, num2) {
+
+}
+
+// EXERCÍCIO 08
+function retornaNPrimeirosPares(n) {
+   
+}
+
+// EXERCÍCIO 09
+function classificaTriangulo(ladoA, ladoB, ladoC) {
+
+}
+
+// EXERCÍCIO 10
+function retornaSegundoMaiorESegundoMenor(array) {
+  
+}
+
+// EXERCÍCIO 11
+function retornaChamadaDeFilme(filme) {
+   
+}
+
+// EXERCÍCIO 12
+function retornaPessoaAnonimizada(pessoa) {
+   
+}
+
+// EXERCÍCIO 13A
+function retornaPessoasAutorizadas(pessoas) {
+   
+}
+
+// EXERCÍCIO 13B
+function retornaPessoasNaoAutorizadas(pessoas) {
+  
+}
+
+// EXERCÍCIO 14
+function retornaContasComSaldoAtualizado(contas) {
+
+}
+
+// EXERCÍCIO 15A
+function retornaArrayOrdenadoAlfabeticamente(consultas) {
+  
+}
+
+// EXERCÍCIO 15B
+function retornaArrayOrdenadoPorData(consultas) {
+   
+}
