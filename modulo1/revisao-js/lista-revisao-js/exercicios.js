77f48633ee2a3d20edf45b1db43e7dbfba0b2580
Mariana Andrade <104591781+Mariandr4de@users.noreply.github.com>
2022-05-23 15:43:51 -0300

    Exercicio 9

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -100,7 +100,13 @@ function retornaNPrimeirosPares(n) {
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-
+    if(ladoA === ladoB && ladoB === ladoC){
+        return "Equilátero"
+    }else if(ladoA === ladoB || ladoB === ladoC || ladoC === ladoA){
+        return "Isósceles"
+    }else{
+        return "Escaleno"
+    }
 }
 
 // EXERCÍCIO 10
