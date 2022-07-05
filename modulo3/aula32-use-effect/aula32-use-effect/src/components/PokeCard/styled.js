f86de1df11c00d9e6d1c35d2616f65ea7f02d8e3
Mariana <mariandrade8115@gmail.com>
2022-07-05 17:17:37 -0300

    use efferct

---
+++ modulo3/aula32-use-effect/aula32-use-effect/src/components/PokeCard/styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const PokeCards = styled.div`
+  font-family: Comic Sans MS;
+  text-align: center;
+
+  select {
+    font-family: Comic Sans MS;
+  }
+`;
+
+export const Card = styled.div`
+  border: 1px solid black;
+  width: 300px;
+  margin: 0 auto;
+  margin-top: 20px;
+
+  img {
+    width: 200px;
+  }
+`;
