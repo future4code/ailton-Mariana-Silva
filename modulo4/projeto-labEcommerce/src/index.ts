4dcd5ee59c36433015558259ad85b5acac66d258
Mariana <mariandrade8115@gmail.com>
2022-09-01 13:29:53 -0300

    correction index

--- modulo4/projeto-labEcommerce/src/index.ts
+++ modulo4/projeto-labEcommerce/src/index.ts
@@ -1,4 +1,4 @@
-import {app} from "./app";
+import { app } from "./app";
 import connectionTest from "./endpoints/connectionTest";
 import { postUsers } from "./endpoints/postUser";
 import { getAllUsers } from "./endpoints/getAllUsers";
@@ -7,10 +7,15 @@ import { getAllProducts } from "./endpoints/getAllProducts";
 import { postPurchase } from "./endpoints/postPurchases";
 import { getAllPurchasesByUser } from "./endpoints/getAllPurchaseByUser";
 
+//Test
 app.get("/connectionTest", connectionTest);
-app.post("/users", postUsers);
+
+//Get
 app.get("/users", getAllUsers);
-app.post("/products", postProduct);
 app.get("/products", getAllProducts);
-app.post("/purchases", postPurchase);
 app.get("/users/:user_id/purchases", getAllPurchasesByUser);
+
+//Post
+app.post("/users", postUsers);
+app.post("/products", postProduct);
+app.post("/purchases", postPurchase);
